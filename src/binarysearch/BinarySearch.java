6c2bb85b7b72342568788f84e36023186d216d3f
Asim Habib <de.asimhabib@gmail.com>
2020-10-15 19:37:31 +0500

    Wrote body of method binarySearch

--- src/binarysearch/BinarySearch.java
+++ src/binarysearch/BinarySearch.java
@@ -3,7 +3,28 @@ package binarysearch;
 
 
 public class BinarySearch {
-
-
     
+    public int binarySearch(int array[] , int value)
+    {
+        int start=0;
+        int end=array.length-1;
+        int foundIndex=-1;
+        
+        while(start<end)
+        {
+            int mid=(start+end)/2;
+            if(array[mid]==value)
+            {
+                foundIndex=mid;
+                break;
+            }
+            
+            else if(value<array[mid])
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        return foundIndex;
+        
+    }
 }
